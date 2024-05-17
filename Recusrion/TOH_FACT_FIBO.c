dcc79f744df79d5a173b568ca17d74bb2256ecad
Nikhil Nigam <nikhilnigam2001@gmail.com>
2024-05-17 11:21:08 +0530

    Update TOH_FACT_FIBO.c
    Add Reverse String Program
    
    Signed-off-by: Nikhil Nigam <nikhilnigam2001@gmail.com>

--- Recusrion/TOH_FACT_FIBO.c
+++ Recusrion/TOH_FACT_FIBO.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
-
+#include<string.h>
 unsigned long long factorial(int n) {
     return (n == 0 || n == 1) ? 1 : n * factorial(n - 1);
 }
 
-void towerOfHanoi(int n, char s, char i, char d) {
+void TOH(int n, char s, char i, char d) {
     if (n == 1) {
-        printf("Rod %c to rod %c\n", s, d);
+        printf("Tower %c to Tower %c\n", s, d);
         return;
     }
-    towerOfHanoi(n - 1, s, d, i);
-    printf("Rod %c to rod %c\n", s, d);
-    towerOfHanoi(n - 1, i, s, d);
+    TOH(n - 1, s, d, i);
+    printf("Tower %c to Tower %c\n", s, d);
+    TOH(n - 1, i, s, d);
 }
 
 unsigned long long fibonacci(int n) {
     return (n <= 1) ? n : fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+void reverseString(char str[], int start, int end) {
+    if (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        reverseString(str, start + 1, end - 1);
+    }
+}
+
+
 int main() {
     int choice, num;
     unsigned long long result;
 
-    while (choice != 4) {
-        printf("\nRecursive Operations Menu:\n"
+    while (choice != 5) {
+        printf("Recursive Operations Menu:\n"
                "1. Factorial\n"
                "2. Tower of Hanoi\n"
                "3. Fibonacci Series\n"
-               "4. Exit\n"
+               "4. Reverse String\n"
+               "5. Exit\n"
                "Enter your choice: ");
         scanf("%d", &choice);
 
@@ -45,19 +56,28 @@ int main() {
                     printf("Number of disks must be a positive integer.\n");
                 } else {
                     printf("Tower of Hanoi with %d disks:\n", num);
-                    towerOfHanoi(num, 'A', 'B', 'C');
+                    TOH(num, 'A', 'B', 'C');
                 }
                 break;
             case 3:
                 printf("Enter the number of terms: ");
                 scanf("%d", &num);
-                printf("Fibonacci Series with %d terms:\n", num);
+                printf("Fibonacci %d terms:\n", num);
                 for (int i = 0; i < num; i++) {
                     printf("%llu ", fibonacci(i));
                 }
                 printf("\n");
                 break;
             case 4:
+                {
+                    char str[100];
+                    printf("Enter a string to reverse: ");
+                    scanf("%s", str);
+                    reverseString(str, 0, strlen(str) - 1);
+                    printf("Reversed string: %s\n", str);
+                }
+                break;
+            case 5:
                 printf("Exit\n");
                 break;
             default:
@@ -67,5 +87,3 @@ int main() {
 
     return 0;
 }
-
-
